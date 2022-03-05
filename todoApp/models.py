5cad9ab3baf16d6e6d2021dee4f204a2048d254e
miker-bice <mikejerrson.galindez@gmail.com>
2022-03-05 18:38:51 +0800

    model for todo items added and migrated

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+# This model holds all the todo items
+class TodoList(models.Model):
+    item = models.TextField(max_length=200, blank=False)
