13153a5704306a440e14ac52961035fe5083f47b
miker-bice <mikejerrson.galindez@gmail.com>
2022-03-10 15:00:41 +0800

    styling for the register form almost done

--- loginApp/forms.py
+++ loginApp/forms.py
@@ -14,18 +14,20 @@ class UserRegistrationForm(UserCreationForm):
                          "rounded-lg focus:ring-blue-500 focus:border-blue-500 block "
                          "w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 "
                          "dark:placeholder-gray-400 dark:text-white "
-                         "dark:focus:ring-blue-500 dark:focus:border-blue-500"}),
+                         "dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                'autocomplete': 'off'}),
             'email': forms.EmailInput(attrs={
                 'class': "bg-gray-50 border border-gray-300 text-gray-900 text-sm "
                          "rounded-lg focus:ring-blue-500 focus:border-blue-500 block "
                          "w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 "
                          "dark:placeholder-gray-400 dark:text-white "
                          "dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                'placeholder': "samplemail@email.com"}),
+                'placeholder': "samplemail@email.com",
+                'autocomplete': 'off'}),
             'password1': forms.PasswordInput(attrs={
                 'class': "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 "
                          "focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 "
-                         "dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 "
+                         "dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500"
                          "dark:focus:border-blue-500"}),
             'password2': forms.PasswordInput(attrs={
                 'class': "bg-gray-50 border border-gray-300 text-gray-900 text-sm "
