a9f32090dc6df1d8cd6fbb7bfec4eb799e0bc17e
miker-bice <mikejerrson.galindez@gmail.com>
2022-03-14 17:24:34 +0800

    some minor styling changes in the todoApp task page

--- loginApp/forms.py
+++ loginApp/forms.py
@@ -18,7 +18,7 @@ class UserRegistrationForm(UserCreationForm):
                          "dark:focus:ring-blue-500 dark:focus:border-blue-500",
                 'autocomplete': 'off'}),
             'email': forms.EmailInput(attrs={
-                'class': " border border-gray-300 text-gray-900 text-sm "
+                'class': "border border-gray-300 text-gray-900 text-sm "
                          "rounded-lg focus:ring-blue-500 focus:border-blue-500 block "
                          "w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 "
                          "dark:placeholder-gray-400 dark:text-white "
