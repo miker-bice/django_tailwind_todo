e4ed5a53d0e31e4c950f0de38fc222b2ca05fb9e
miker-bice <mikejerrson.galindez@gmail.com>
2022-03-22 20:43:23 +0800

    some change

--- django_tailwind_todo/settings.py
+++ django_tailwind_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5t7-2ll5trow#53308hyrt69d#pgb1(vcmf&@f@0=zo+u-v&+2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
